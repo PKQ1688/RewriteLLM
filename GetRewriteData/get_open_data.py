4b83e9bb2c51b3df2e592d8700bf06264a3a282c
pkq1688 <adolf1321794021@gmail.com>
2023-04-20 14:24:20 +0000

    update

---
+++ GetRewriteData/get_open_data.py
@@ -0,0 +1,8 @@
+"""
+@Author       : pkq1688
+@email        : adolf1321794021@gmail.com
+@Date         : 2023-04-20 14:23:34
+@LastEditors  : pkq1688
+@LastEditTime : 2023-04-20 14:23:47
+@Description  : 
+"""
